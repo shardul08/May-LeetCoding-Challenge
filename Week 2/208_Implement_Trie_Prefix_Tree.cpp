12cce49676ba7765ca1006e2a1a7ba0338eb37b8
Shardul Silswal <sshardul007@gmail.com>
2020-05-14 12:49:22 +0530

    May 14

---
+++ Week 2/208_Implement_Trie_Prefix_Tree.cpp
@@ -0,0 +1,75 @@
+/*
+Implement a trie with insert, search, and startsWith methods.
+
+Example:
+
+Trie trie = new Trie();
+
+trie.insert("apple");
+trie.search("apple");   // returns true
+trie.search("app");     // returns false
+trie.startsWith("app"); // returns true
+trie.insert("app");   
+trie.search("app");     // returns true
+
+Note:
+
+    You may assume that all inputs are consist of lowercase letters a-z.
+    All inputs are guaranteed to be non-empty strings.
+
+*/
+
+// Solution
+
+    class Trie {
+public:
+    /** Initialize your data structure here. */
+    Trie() {
+        
+    }
+    
+    /** Inserts a word into the trie. */
+    void insert(string word) {
+        Trie* node = this;
+        for (char ch : word) {
+            ch -= 'a';
+            if (!node->next[ch]) { node->next[ch] = new Trie(); }
+            node = node->next[ch];
+        }
+        node->isword = true;        
+    }
+    
+    /** Returns if the word is in the trie. */
+    bool search(string word) {
+        Trie* node = this;
+        for (char ch : word) {
+            ch -= 'a';
+            if (!node->next[ch]) { return false; }
+            node = node->next[ch];
+        }
+        return node->isword;        
+    }
+    
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    bool startsWith(string prefix) {
+        Trie* node = this;
+        for (char ch : prefix) {
+            ch -= 'a';
+            if (!node->next[ch]) { return false; }
+            node = node->next[ch];
+        }
+        return true;
+    }
+    
+private:
+    Trie* next[26] = {};
+    bool isword = false;
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
